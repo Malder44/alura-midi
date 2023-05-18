987407b954b7860c1694f77207d8e91aee88766b
Leonorico <leonorico2epb@gmail.com>
2023-05-18 15:36:13 -0300

    Criando função e aplicando a ação de tocar som ao clicar no botão Pom

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert('Olá mundo');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+    return 0;
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+    return 0;
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
