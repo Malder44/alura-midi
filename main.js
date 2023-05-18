b17cd414de31a35aabf9e28cee86381ae2a5bca0
Leonorico <leonorico2epb@gmail.com>
2023-05-18 15:54:26 -0300

    Criando uma lista para armazenar todos os botões e lidando com o primeiro elemento (tecla pom) e seu som

--- main.js
+++ main.js
@@ -8,4 +8,5 @@ function tocaSomClap() {
     return 0;
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
