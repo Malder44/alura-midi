49b0568a77981bfff5527cd2393716b40d8f06f5
Leonorico <leonorico2epb@gmail.com>
2023-05-24 19:01:58 -0300

    Adição de interações via teclado, condições e eventos

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
-    return 0;
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-    return 0;
+function adicionaEstilo(tecla) {
+    tecla.classList.add('ativa');
+}
+
+function removeEstilo(tecla) {
+    tecla.classList.remove('ativa');
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    };
+    tecla.onkeydown = function(evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+            adicionaEstilo(tecla);
+        }
+    }
+    tecla.onkeyup = function() {
+        removeEstilo(tecla);
+    }
+}
